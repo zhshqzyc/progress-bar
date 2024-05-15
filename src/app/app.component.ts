792793f4be7eaf84b7cb3807334c691011fe4206
Hui Zhao <zhshqzyc@gmail.com>
2024-05-15 14:06:53 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ProgressBarComponent],
 })
 export class AppComponent {
-  title = 'progress-bar';
+  max: number = 100;
+  value: number = 40;
 }
